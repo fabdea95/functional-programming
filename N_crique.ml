cf7c469f53cac03744ce2102c11a7bf4441fccd2
fabdea95 <61844644+fabdea95@users.noreply.github.com>
2020-10-18 16:58:22 +0200

    Update N_crique.ml

--- N_crique.ml
+++ N_crique.ml
@@ -8,27 +8,15 @@ let searchp_base results inizio n (Graph s)=
                                 | cammino::rest -> if (List.length cammino) = n then search_aux (results @ [cammino]) rest
                                                    else search_aux results ((estendi cammino) @ rest)
                               in search_aux results [[inizio]];;
-(*COMMENTO:
-START:
-CHIAMA SEARCH AUX:
-   controlla  "inizio":
-	se vuoto -> results
-	altrimenti controlla: SE inizio è una lista lunga N ALLORA aggiunge il cammino ai risultati ed esegue search_aux sulla coda
-        	ALTRIMENTI richiama search_aux passandogli results e il risultato della concatenazione tra 
-									ESTENDI cammino e la coda della lista "inizio"
-	FUNZIONE ESTENDI:
-	riceve cammino
-	Filtra la lista dei successori dell'ultimo nodo visitato (ovvero List.hd cammino) selezionando solo quelli non ancora visitati nel cammino, 
-	e per ogni nodo x trovato richiama search_aux passando x::cammino come "lista"
-*)
-	(*lista prima chiamato inizio*)
+
+
 	
 
 
 
 (*N-CRIQUE PATHS FINDER*)
 let searchp results inizio n (Graph s)=
-                let estendi cammino = (*if (List.length cammino) = n then*) stampalista [inizio];  
+                let estendi cammino = 
 				List.map (function x -> x::cammino)
                                 (List.filter (function x -> not (List.mem x cammino)) (s (List.hd cammino)))
                         in let rec search_aux results lista  = match lista with
@@ -38,16 +26,30 @@ let searchp results inizio n (Graph s)=
 										     else search_aux results rest
                                                    else search_aux results ((estendi cammino) @ rest)
                               in search_aux results [[inizio]];;
-			
+
+(*COMMENTO:
+START:
+CHIAMA SEARCH AUX:
+   controlla  "inizio":
+	se vuoto -> results
+	altrimenti controlla: SE inizio è una lista lunga N ALLORA controlla SE l'ultimo nodo è collegato al nodo iniziale del cammino:
+											 se SÌ, aggiunge il cammino ai risultati ed esegue search_aux sulla coda
+											 ALTRIMENTI non lo aggiunge
+        	ALTRIMENTI richiama search_aux passandogli results e il risultato della concatenazione tra ESTENDI cammino e la coda della lista "inizio"
+
+	FUNZIONE ESTENDI:
+	riceve cammino
+	Filtra la lista dei successori dell'ultimo nodo visitato (ovvero List.hd cammino) selezionando solo quelli non ancora visitati nel cammino, 
+	e per ogni nodo x trovato richiama search_aux passando x::cammino come "lista"
+*)
+
 
 exception NotFound;;
 
-(*CHECK IF N-CRIQUE*)
 let rec check inizio n (Graph g) =
 	if (g inizio) = [] then (print_endline("nessuna cricca"); raise NotFound )
 	else let cammini = (searchp [] inizio n (Graph g)) in if (List.length cammini) >= (n-1) then (List.hd cammini)
 		else check (succ inizio) n (Graph g);;
 
-(*START PROGRAM*)
 let cricca n (Graph g) =
    check 1 n (Graph g);;
