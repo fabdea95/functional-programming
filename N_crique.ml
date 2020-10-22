17928eeb0fec7f7c41ed1d5ee0dc31cef98abd6c
fabdea95 <61844644+fabdea95@users.noreply.github.com>
2020-10-22 18:21:57 +0200

    completely works

--- N_crique.ml
+++ N_crique.ml
@@ -10,10 +10,37 @@ let searchp_base results inizio n (Graph s)=
                               in search_aux results [[inizio]];;
 
 
-	
+(*N-CRIQUE PATHS FINDER*)
+let searchp results cont inizio n (Graph s)=
+    let estendi cammino = 
+	List.map (function x -> x::cammino)
+        (List.filter (function x -> not (List.mem x cammino)) (s (List.hd cammino)))
+             in let rec search_aux results cont lista  = match lista with
+                 [] -> []
+                 | cammino::rest -> if (List.length cammino) = n then if (List.mem inizio (s (List.hd cammino))) 
+									then if n = 2 then []@[cammino]
+									else let cammino_sort = (List.sort compare cammino) in 
+	    								     let indice = find cammino_sort results in 
+											if indice > -1 then 
+							       				    let nuovo_cont = ((List.nth cont indice) + 1) in if nuovo_cont = n-1 then ([] @ [cammino_sort])
+											   						     else search_aux results (replace cont indice nuovo_cont) rest
+						          				(* else if n=2 then []@[cammino_sort ]*)
+												else search_aux (results @ [cammino_sort]) (cont @ [1]) rest
+									else search_aux results cont rest
+                                    else search_aux results cont ((estendi cammino) @ rest)
+              in search_aux results cont [[inizio]];;
+	      
+exception NotFound;;
+let rec check inizio n (Graph g) =
+	if (g inizio) = [] then (print_endline("nessuna cricca della dimensione inserita"); raise NotFound )
+	else let cammini = (searchp [] [] inizio n (Graph g)) in if (List.length cammini) > 0 then (cammini)
+		else check (succ inizio) n (Graph g);;
+
+let cricca n (Graph g) =
+   check 1 n (Graph g);;
 
 
-
+(*OLD VERSION*
 (*N-CRIQUE PATHS FINDER*)
 let searchp results inizio n (Graph s)=
                 let estendi cammino = 
@@ -53,3 +80,4 @@ let rec check inizio n (Graph g) =
 
 let cricca n (Graph g) =
    check 1 n (Graph g);;
+*)
