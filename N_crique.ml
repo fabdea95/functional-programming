28df7335c2c951bb0250cac7e4d0d68b537f3e4b
fabdea95 <61844644+fabdea95@users.noreply.github.com>
2020-10-26 11:28:08 +0100

    Update N_crique.ml

--- N_crique.ml
+++ N_crique.ml
@@ -1,4 +1,4 @@
-(*TROVA ELEMENTO E RESTITUISCE INDICE*)
+(*TROVA ELEMENTO E RESTITUISCE INDICE, se non lo trova ritorna -1 *)
 let rec func x lst c = match lst with
     | [] -> -1
     | hd::tl -> if (hd=x) then c else func x tl (c+1)
@@ -16,24 +16,43 @@ let dfs_cricca risultati cont inizio n (Graph s)=
              in let rec search_aux risultati cont lista  = match lista with
                  [] -> []
                  | cammino::rest -> if (List.length cammino) = n then 
-		 	if (List.mem inizio (s (List.hd cammino))) then 
-			    if n = 2 then []@[cammino]
-				else let cammino_sort = (List.sort compare cammino) in 
-	    			     let indice = trova_i cammino_sort risultati in 
-					if indice > -1 then 
-	 				    let nuovo_cont = ((List.nth cont indice) + 1) in if nuovo_cont = n-1 then ([] @ [cammino_sort])
-					   						     else search_aux risultati (sostituisci cont indice nuovo_cont) rest
-					else search_aux (risultati @ [cammino_sort]) (cont @ [1]) rest
-		             else search_aux risultati cont rest
-                       else search_aux risultati cont ((estendi cammino) @ rest)
-              in search_aux risultati cont [[inizio]];;
+		 	 		if (List.mem inizio (s (List.hd cammino))) then 
+			    			if n = 2 then []@[cammino]
+						else let cammino_sort = (List.sort compare cammino) in 
+	    			     			let indice = trova_i cammino_sort risultati in 
+							if indice > -1 then 
+	 				    			let nuovo_cont = ((List.nth cont indice) + 1) in 
+									if nuovo_cont = n-1 then ([] @ [cammino_sort])
+					   	 			else search_aux risultati (sostituisci cont indice nuovo_cont) rest
+							else search_aux (risultati @ [cammino_sort]) (cont @ [1]) rest
+		            		 else search_aux risultati cont rest
+                       		     else search_aux risultati cont ((estendi cammino) @ rest)
+     in search_aux risultati cont [[inizio]];
 	      
+(*COMMENTO DFS_CRICCA:
+START:
+CHIAMA SEARCH AUX:
+   controlla  "inizio":
+	se vuoto -> risultati
+	altrimenti controlla: SE cammino è una lista lunga N ALLORA controlla 
+					SE l'ultimo nodo è collegato al nodo iniziale del cammino, ordina la lista dei nodi del cammino, poi:
+						SE la combinazione dei nodi del cammino è gia presente nella lista risultati allora incrementa il valore di cont corrispondente
+						ALTRIMENTI aggiungi cammino_sort risultati, e aggiungi "1" come nuovo elemento in cont ed esegue search_aux sulla coda
+					ALTRIMENTI non lo aggiunge
+        		      ALTRIMENTI richiama search_aux passandogli results e il risultato della concatenazione tra ESTENDI cammino e la coda della lista "inizio"
+
+	FUNZIONE ESTENDI:
+	riceve cammino
+	Filtra la lista dei successori dell'ultimo nodo visitato (ovvero List.hd cammino) selezionando solo quelli non ancora visitati nel cammino, 
+	e per ogni nodo x trovato richiama search_aux passando x::cammino come "lista"
+*)
+
 exception NotFound;;
 (* funzione che richiama ogni volta dfs_cricca per un nuovo vertice di inizio fino a quando non trova una cricca*)
 let rec check inizio n (Graph g) =
-	if (g inizio) = [] then (print_endline("nessuna cricca della dimensione inserita"); raise NotFound )
-	else let cammini = (dfs_cricca [] [] inizio n (Graph g)) in if (List.length cammini) > 0 then (List.hd cammini)
-		else check (succ inizio) n (Graph g);;
+	if (g inizio) = [] then (print_endline("nessuna cricca della dimensione inserita"); raise NotFound )		(*se il vertice non ha vicini -> notfound*)
+	else let cammini = (dfs_cricca [] [] inizio n (Graph g)) in if (List.length cammini) > 0 then (List.hd cammini) (*se dfs_cricca trova una cricca, ritornala *)
+		else check (succ inizio) n (Graph g);;									(*altrimenti richiama check sul prossimo nodo*)
 
 exception InputNotCorrect;;
 (*funzione main*)
